f0388e2eab4a0078fec694e46dada86f905d63d5
Aslam <12aslam213@gmail.com>
2022-02-19 19:47:21 +0530

    Maven Project

---
+++ src/main/java/com/anime/first_demo/App.java
@@ -0,0 +1,13 @@
+package com.anime.first_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
